b8a96ce19f92ecec7d0d8bd56be0d1eb509a8f5e
Pruthvi <pruthvi@users.noreply.github.com>
2018-09-21 17:51:11 -0400

    Basic Master Page set

--- MyCondo/MasterPage.Master.designer.cs
+++ MyCondo/MasterPage.Master.designer.cs
@@ -22,21 +22,21 @@ namespace MyCondo {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// ContentPlaceHolder2 control.
+        /// PageTitle control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder PageTitle;
         
         /// <summary>
-        /// ContentPlaceHolder3 control.
+        /// PageContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder3;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder PageContent;
     }
 }
--- MyCondo/index.aspx
+++ MyCondo/index.aspx
@@ -1,6 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="MyCondo.index" %>
 
-<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+<asp:Content ID="Content3" ContentPlaceHolderID="PageTitle" runat="server"> Home Page
 </asp:Content>
-<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+<asp:Content ID="Content4" ContentPlaceHolderID="PageContent" runat="server">
+    Enter Page Content here
 </asp:Content>
