7cb8d5c2600284d77eea0efb0a15ec995b60a6d5
Pruthvi <pruthvi@users.noreply.github.com>
2018-09-21 18:27:14 -0400

    Added Login Page

---
+++ MyCondo/login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCondo
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtPassword.Text == "admin")
+            {
+                Response.Redirect("index.aspx");
+
+            }
+            else
+            {
+                lblValidation.Text = "Your username and password is incorrect";
+                lblValidation.ForeColor = System.Drawing.Color.Red; 
+            }
+        }
+    }
+}
