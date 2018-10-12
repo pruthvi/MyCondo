bcc43b0c62657d0c3f8458c8d399af5243ac5414
hkoflax <hkoflax@users.noreply.github.com>
2018-10-12 17:12:01 -0400

    Creating database
    new database

---
+++ MyCondo/Database Scripts/Create tables in Mycondo.sql
@@ -0,0 +1,9 @@
+﻿USE [Mycondo]
+GO
+CREATE TABLE [dbo].[login]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL,
+	[UserType] NVARCHAR(50) NOT NULL
+)ON [PRIMARY]
