0dfd19eaafd2e0e07ceb7810928a78906a3f067c
Pruthvi <pruthvi@users.noreply.github.com>
2018-12-07 13:47:38 -0500

    Added Guest Registration

---
+++ MyCondo/GuestRegistration.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCondo
+{
+    public partial class GuestRegistration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateGuest_Click(object sender, EventArgs e)
+        {
+        //    GuestRegistration newGuest = new GuestRegistration();
+        ////            private string guestName;
+        ////private int hostId;
+        ////private string visitorType;
+        ////private bool parking;
+
+        //newGuest.GUes = txtName.Text;
+
+        //    myBooking.UserId = Convert.ToInt32(ResidentDropDown.SelectedValue);
+
+
+        //        try
+        //        {
+        //            SqlCommand command = new SqlCommand();
+        //            command.CommandText = "InsertBooking";
+        //            command.CommandType = CommandType.StoredProcedure;
+
+        //            command.Parameters.Add("BookingType", SqlDbType.NVarChar);
+        //            command.Parameters["BookingType"].Value = myBooking.BookingType;
+        //            command.Parameters.Add("StartDate", SqlDbType.DateTime);
+        //            command.Parameters["StartDate"].Value = myBooking.StartDate;
+        //            command.Parameters.Add("EndDate", SqlDbType.DateTime);
+        //            command.Parameters["EndDate"].Value = myBooking.EndDate;
+        //            command.Parameters.Add("BookingDescription", SqlDbType.NVarChar);
+        //            command.Parameters["BookingDescription"].Value = myBooking.Description;
+        //            command.Parameters.Add("UserId", SqlDbType.Int);
+        //            command.Parameters["UserId"].Value = myBooking.UserId;
+
+        //            DataConnection myConnection = new DataConnection();
+        //            myConnection.ExecuteNonQuery(command);
+        //            Response.Redirect("Home.aspx");
+        //            lblDatealreadyBooked.Visible = false;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Response.Write(ex.Message);
+        //            {
+        //            }
+        //        }
+
+        }
+    }
+}
